251a7588128e4a9d8b50e78b9af80cc45d910bd2
nihatonal <onalnihat1986@gmail.com>
2023-01-16 12:49:47 +0300

    updated

--- src/AboutUs/components/ImageGallery.js
+++ src/AboutUs/components/ImageGallery.js
@@ -7,7 +7,7 @@ function ImageGallery(props) {
         <div className='images-container'>
             <h2 className="images-title">{title}</h2>
             <div className='images-wrapper'>
-                {images.map(image => <img src={image.image} alt='image_gallery' />)}
+                {images.map(image => <div className='image-gallery-item-wrapper'> <img src={image.image} alt='image_gallery' /></div>)}
                 <div className="image-content">
                     <p className="image-content-desc">{image_content}</p>
                 </div>
--- src/shared/UI/Slider.js
+++ src/shared/UI/Slider.js
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import ArrowRight from '../../assets/icons/mini_slide_arrow-right.svg';
 import ArrowLeft from '../../assets/icons/mini_slide_arrow-left.svg'
 import { ReactSVG } from 'react-svg';
 import './Slider.css';
 const Slider = ({ slides }) => {
+    const ref = useRef(null);
     const [current, setCurrent] = useState(0);
     const length = slides.length;
-
+    const [widthSlide, setWidthSlide] = useState(546)
+    useEffect(() => {
+        setWidthSlide(ref.current.offsetWidth)
+    }, [ref.current]);
     const nextSlide = () => {
         setCurrent(current === length - 1 ? 0 : current + 1);
     };
@@ -40,7 +44,7 @@ const Slider = ({ slides }) => {
                 />
                 <p>0{slides.length}</p>
             </div>
-            <div className='slide-wrapper' style={{ left: `${-546 * current}px` }}>
+            <div ref={ref} className='slide-wrapper' style={{ left: `${-widthSlide * current}px` }}>
                 {slides.map((slide, index) => {
                     return (
                         <div
